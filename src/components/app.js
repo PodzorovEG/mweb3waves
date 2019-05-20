3a9163cb23beeedb054024dd69fea23906ea3051
PodzorovEG <50666755+PodzorovEG@users.noreply.github.com>
2019-05-20 10:59:59 +0700

    add project files

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom";
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="container">
+    		    <input className="btn btn-primary" type="submit" value="Утро - " 
+                onClick={() => {alert("наверно - доброе!");}}/>
+    	    </div>
+        )
+    }
+}
+
+const app = document.getElementById('app');
+if(app){
+    ReactDOM.render(<App/>, app);
+}
